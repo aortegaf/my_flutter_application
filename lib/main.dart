0e2a1cc325c456f21502c8150b101849706e575c
aortegaf <aortegaf@unal.edu.co>
2022-07-14 00:04:20 -0500

    Show/Hide Widget

--- lib/main.dart
+++ lib/main.dart
@@ -9,129 +9,190 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Startup Name Generator',
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Startup Name Generator'), actions: [
-          IconButton(
-            icon: const Icon(Icons.camera_alt_rounded),
-            tooltip: 'Camera Permission',
-            onPressed: () {
-              cameraPermission();
-            },
-          ),
-        ]),
-        body: const Center(
-          child: RandomWords(),
+      title: 'Show/Hide Widget',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool isVisible = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Show/Hide Widget'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Visibility(
+              visible: isVisible,
+              maintainSize: true,
+              maintainAnimation: true,
+              maintainState: true,
+              child: Container(
+                width: 300,
+                height: 300,
+                color: Colors.green,
+              ),
+            ),
+            RaisedButton(
+              child: Text('Show/Hide'),
+              onPressed: () {
+                setState(() {
+                  isVisible = !isVisible;
+                });
+              },
+            ),
+          ],
         ),
       ),
     );
   }
 }
 
-class Alert extends StatefulWidget {
-  AlertState createState() => AlertState();
-}
+// void main() {
+//   runApp(const MyApp());
+// }
 
-class AlertState extends State {
-  @override
-  void initState() {
-    showAlert(context);
-    super.initState();
-  }
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
-  Future showAlert(BuildContext context) async {
-    await Future.delayed(Duration(seconds: 6));
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Startup Name Generator',
+//       home: Scaffold(
+//         appBar: AppBar(title: const Text('Startup Name Generator'), actions: [
+//           IconButton(
+//             icon: const Icon(Icons.camera_alt_rounded),
+//             tooltip: 'Camera Permission',
+//             onPressed: () {
+//               cameraPermission();
+//             },
+//           ),
+//         ]),
+//         body: const Center(
+//           child: RandomWords(),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: new Text('Welcome To Our App :) .'),
-          actions: <Widget>[
-            FlatButton(
-              child: new Text("OK"),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
+// class Alert extends StatefulWidget {
+//   AlertState createState() => AlertState();
+// }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-          child: Text(
-        'Call A Function Automatically When App Starts Everytime',
-        style: TextStyle(
-          fontSize: 22,
-        ),
-        textAlign: TextAlign.center,
-      )),
-    );
-  }
-}
+// class AlertState extends State {
+//   @override
+//   void initState() {
+//     showAlert(context);
+//     super.initState();
+//   }
 
-class RandomWords extends StatefulWidget {
-  const RandomWords({Key? key}) : super(key: key);
+//   Future showAlert(BuildContext context) async {
+//     await Future.delayed(Duration(seconds: 6));
 
-  @override
-  State<RandomWords> createState() => _RandomWordsState();
-}
+//     showDialog(
+//       context: context,
+//       builder: (BuildContext context) {
+//         return AlertDialog(
+//           title: new Text('Welcome To Our App :) .'),
+//           actions: <Widget>[
+//             FlatButton(
+//               child: new Text("OK"),
+//               onPressed: () {
+//                 Navigator.of(context).pop();
+//               },
+//             ),
+//           ],
+//         );
+//       },
+//     );
+//   }
 
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18);
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//           child: Text(
+//         'Call A Function Automatically When App Starts Everytime',
+//         style: TextStyle(
+//           fontSize: 22,
+//         ),
+//         textAlign: TextAlign.center,
+//       )),
+//     );
+//   }
+// }
 
-  @override
-  void initState() {
-    super.initState();
-    cameraPermission();
-  }
+// class RandomWords extends StatefulWidget {
+//   const RandomWords({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: /*1*/ (context, i) {
-        if (i.isOdd) return const Divider(); /*2*/
+//   @override
+//   State<RandomWords> createState() => _RandomWordsState();
+// }
 
-        final index = i ~/ 2; /*3*/
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-        }
-        return ListTile(
-          title: Text(
-            _suggestions[index].asPascalCase,
-            style: _biggerFont,
-          ),
-        );
-      },
-    );
-  }
-}
+// class _RandomWordsState extends State<RandomWords> {
+//   final _suggestions = <WordPair>[];
+//   final _biggerFont = const TextStyle(fontSize: 18);
 
-cameraPermission() async {
-  var cameraStatus = await Permission.camera.status;
-  print(cameraStatus);
+//   @override
+//   void initState() {
+//     super.initState();
+//     cameraPermission();
+//   }
 
-  if (!cameraStatus.isGranted) await Permission.camera.request();
+//   @override
+//   Widget build(BuildContext context) {
+//     return ListView.builder(
+//       padding: const EdgeInsets.all(16.0),
+//       itemBuilder: /*1*/ (context, i) {
+//         if (i.isOdd) return const Divider(); /*2*/
 
-  if (await Permission.camera.isGranted) {
-    log("Camera permission granted");
-  } else {
-    log("Provide Camera permission to use camera.");
-  }
-}
+//         final index = i ~/ 2; /*3*/
+//         if (index >= _suggestions.length) {
+//           _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+//         }
+//         return ListTile(
+//           title: Text(
+//             _suggestions[index].asPascalCase,
+//             style: _biggerFont,
+//           ),
+//         );
+//       },
+//     );
+//   }
+// }
+
+// cameraPermission() async {
+//   var cameraStatus = await Permission.camera.status;
+//   print(cameraStatus);
+
+//   if (!cameraStatus.isGranted) await Permission.camera.request();
+
+//   if (await Permission.camera.isGranted) {
+//     log("Camera permission granted");
+//   } else {
+//     log("Provide Camera permission to use camera.");
+//   }
+// }
